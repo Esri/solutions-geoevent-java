14b916a537284dba12d303a1e480923dafe260a8
Patrick Hill <patrick_hill@esri.com>
2014-01-28 11:56:28 -0600

    Add License headers

--- solutions-geoevent/adapters/cot-adapter/src/main/java/com/esri/geoevent/solutions/adapter/cot/CoTInboundAdapterDefinition.java
+++ solutions-geoevent/adapters/cot-adapter/src/main/java/com/esri/geoevent/solutions/adapter/cot/CoTInboundAdapterDefinition.java
@@ -1,5 +1,29 @@
 package com.esri.geoevent.solutions.adapter.cot;
 
+/*
+ * #%L
+ * CoTInboundAdapterDefinition.java - Esri :: AGES :: Solutions :: Adapter :: CoT - Esri - 2013
+ * org.codehaus.mojo-license-maven-plugin-1.5
+ * $Id: update-file-header-config.apt.vm 17764 2012-12-12 10:22:04Z tchemit $
+ * $HeadURL: https://svn.codehaus.org/mojo/tags/license-maven-plugin-1.5/src/site/apt/examples/update-file-header-config.apt.vm $
+ * %%
+ * Copyright (C) 2013 - 2014 Esri
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
